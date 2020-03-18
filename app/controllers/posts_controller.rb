847427bd7046f919b076ca7f8e751e228b216a57
shuma1127 <shuma.1127b@gmail.com>
2020-03-18 14:46:20 +0900

    投稿機能のルーティング

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,20 @@
 class PostsController < ApplicationController
+
   def index
     @posts = Post.all
   end
+
+  def new
+    @tweet = Post.new
+  end
+
+  def create
+    Post.create(posts_params)
+  end
+
+  private
+  def tweet_params
+    params.permit(:name, :image, :title)
+  end
+
 end
