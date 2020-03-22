58c0b15c80ca7ba3012d09bc81ab84c5c304f90d
shuma1127 <shuma.1127b@gmail.com>
2020-03-22 17:10:03 +0900

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,5 @@ class UsersController < ApplicationController
     user = User.find(params[:id])
     @nickname = user.nickname
     @posts = user.posts.page(params[:page]).per(8).order("created_at DESC")
-    # @posts = user.posts.page(params[:page]).per(8).order("created_at DESC")
   end
 end
