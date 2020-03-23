16edbbe77f8d3942737685e877aa50cea55bc6b8
shuma1127 <shuma.1127b@gmail.com>
2020-03-23 19:47:23 +0900

    Revert "view 途中"
    This reverts commit 276fd50c9f794a7b11d6a34d8110464d38890507.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,12 +2,4 @@ class Post < ApplicationRecord
   validates :image, :title, :word, presence: true
   mount_uploader :image, ImageUploader
   belongs_to :user
-
-  def self.search(search)
-    if search
-      Post.where('text LIKE(?)', "%#{search}%")
-    else
-      Post.all
-    end
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
-  resources :posts 
-  namespace :posts do
-    resources :searches, only: :index
-end
+  resources :posts
   resources :users, only: :show
 end
