2021ffaea81d264436c9eee43483d6ee1d5e5e58
shuma1127 <shuma.1127b@gmail.com>
2020-03-23 11:32:20 +0900

    ジャンルの追加

--- db/migrate/20200318012434_create_posts.rb
+++ db/migrate/20200318012434_create_posts.rb
@@ -4,6 +4,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
       t.string :where
       t.string :word
       t.string :title
+      t.string :genre
       t.string :review
       t.string :image
       t.timestamps
