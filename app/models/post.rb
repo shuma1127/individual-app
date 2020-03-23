276fd50c9f794a7b11d6a34d8110464d38890507
shuma1127 <shuma.1127b@gmail.com>
2020-03-23 19:15:31 +0900

    view 途中

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   validates :image, :title, :word, presence: true
   mount_uploader :image, ImageUploader
   belongs_to :user
+
+  def self.search(search)
+    if search
+      Post.where('text LIKE(?)', "%#{search}%")
+    else
+      Post.all
+    end
+  end
 end
