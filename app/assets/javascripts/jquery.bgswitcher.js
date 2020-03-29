16948b11036346b4339fb639fb095c66ff194dd5
shuma1127 <shuma.1127b@gmail.com>
2020-03-29 22:13:50 +0900

    backgraund-imag(画像の追加)

--- app/assets/javascripts/jquery.bgswitcher.js
+++ app/assets/javascripts/jquery.bgswitcher.js
@@ -11,13 +11,13 @@
 
 jQuery(function($) {
   $('.account').bgSwitcher({
-      images: ['/1.jpg','/2.jpg','/3.jpg','/4.jpg'], 
-    interval: 2000, 
-      loop: true, 
-      shuffle: true, 
-      effect: "blind", 
-      duration: 500, 
-      easing: "swing" 
+    images: ['/1.jpg','/2.jpg','/3.jpg','/4.jpg','/5.jpg',], // 切替背景画像を指定
+    interval: 5000, // 背景画像を切り替える間隔を指定 3000=3秒
+    loop: true, // 切り替えを繰り返すか指定 true=繰り返す　false=繰り返さない
+    shuffle: true, // 背景画像の順番をシャッフルするか指定 true=する　false=しない
+    effect: "fade", // エフェクトの種類をfade,blind,clip,slide,drop,hideから指定
+    duration: 2000, // エフェクトの時間を指定します。
+    easing: "linear" // エフェクトのイージングをlinear,swingから指定
     });
   });
 
