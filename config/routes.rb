88b565973a1f1c6109edcfcedd07b83ab0246d6c
shuma1127 <shuma.1127b@gmail.com>
2020-04-11 21:01:16 +0900

    ターボリンクス
    ログイン
    ログアウト解除

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
-  resources :posts do
+  resources :posts, shallow: true do
     collection do
       get 'search'
     end
@@ -9,13 +9,3 @@ Rails.application.routes.draw do
   resources :users, only: :show
 end
 
-# devise_for :users
-#   root to: 'tweets#index'
-#   resources :tweets do
-#     resources :comments, only: :create
-#     collection do
-#       get 'search'
-#     end
-#   end
-#   resources :users, only: :show
-# end
