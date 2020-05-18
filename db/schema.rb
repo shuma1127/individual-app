53b00b339acdfc4bdbd9307fd265321297f32d3a
shuma1127 <shuma.1127b@gmail.com>
2020-05-18 21:46:30 +0900

    フォームの修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_16_022630) do
+ActiveRecord::Schema.define(version: 2020_05_18_123929) do
 
   create_table "bookmarks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -21,13 +21,20 @@ ActiveRecord::Schema.define(version: 2020_04_16_022630) do
     t.index ["user_id"], name: "index_bookmarks_on_user_id"
   end
 
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "src"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_images_on_post_id"
+  end
+
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "place"
     t.string "word"
     t.string "title"
     t.string "genre"
     t.string "review"
-    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
@@ -48,4 +55,5 @@ ActiveRecord::Schema.define(version: 2020_04_16_022630) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "images", "posts"
 end
